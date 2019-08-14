e13d83c250e6a7a11d2503ca76da197b7f972783
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-08-14 20:06:16 +0700

    done task5

--- exercise-5.js
+++ exercise-5.js
@@ -1,13 +1,28 @@
-// function ubahHuruf(kata) {
-    var kamus = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    
-    
-    
-//   }
+function ubahHuruf(kata) {
+  var dict = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  var result = []
+  var temp = []
+  temp = kata.split('')
+
+  for (var i = 0; i < dict.length; i++) {
+      for (var j = 0; j < temp.length; j++) {
+          if (dict[i] === temp[j]) {
+             temp.splice(j, 1, i + 1)
+          }
+      }
+      
+  }
+  for (var k = 0; k < temp.length; k++) {
+      result += dict[temp[k]]
+  }
+  return result
   
-  // TEST CASES
-//   console.log(ubahHuruf('wow')); // xpx
-//   console.log(ubahHuruf('developer')); // efwfmpqfs
-//   console.log(ubahHuruf('javascript')); // kbwbtdsjqu
-//   console.log(ubahHuruf('keren')); // lfsfo
-//   console.log(ubahHuruf('semangat')); // tfnbohbu
+}
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+  console.log(ubahHuruf('developer')); // efwfmpqfs
+  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+  console.log(ubahHuruf('keren')); // lfsfo
+  console.log(ubahHuruf('semangat')); // tfnbohbu
+
